b5451690fa0ffb5af60320f3e86adac609fdfa66
stud305 <stud305@PC9-305>
2021-12-06 15:20:06 +0300

    asdasd

---
+++ OlegGoodMan/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlegGoodMan
+{
+    public class Class1
+    {
+        public Double Sum(double? a, int b, decimal c) {
+
+          Double  a1 = Convert.ToDouble(a);
+            Double b1 = Convert.ToDouble(b);
+            Double c1 = Convert.ToDouble(c);
+
+            return a1*b1*c1;
+
+        }
+    }
+}
